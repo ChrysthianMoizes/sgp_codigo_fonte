4666991cffb8970827f38614dee342a43179f210
Alisson Vighini <alisson.vighini@gmail.com>
2020-04-29 16:45:30 -0300

    adicionando breadcrumb

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import {BreadcrumbModule} from 'primeng/breadcrumb';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, BreadcrumbComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BreadcrumbModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
