f0a99014b08fd4ed3c91954ed7b44bcd0973ae31
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-04-29 16:14:53 -0300

    Componente criado

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfillllComponent } from './components/perfillll/perfillll.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, PerfilComponent, PerfillllComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
