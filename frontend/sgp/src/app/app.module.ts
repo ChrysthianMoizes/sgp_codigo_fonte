b2efab900287caab68d8d641daca1a5036094c2f
Flávio L <flaviojunior0899@gmail.com>
2020-04-29 17:03:43 -0300

    importing layout component to app.module

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
