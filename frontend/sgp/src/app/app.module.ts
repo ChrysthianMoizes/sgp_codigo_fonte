2867e551549636a372f204f28f21e68731875070
Flávio L <flaviojunior0899@gmail.com>
2020-04-30 10:51:42 -0300

    imported nav bar into app.module

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LayoutComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, LayoutComponent, NavBarComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, MenuModule],
   providers: [],
   bootstrap: [AppComponent],
 })
