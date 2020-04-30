8452bce893fd151c9ae8acc1be0bddbc60e7b0d1
Tito Stauffer <tito.stauffer@gmail.com>
2020-04-30 11:09:56 -0300

    alert criado, faltando implementar algumas coisas

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -1,13 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './components/alert/alert.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, AlertComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    ToastModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
