607eaccbb8b46442e7387c08043c00adfbe3d2c3
Flávio L <flaviojunior0899@gmail.com>
2020-05-04 13:24:45 -0300

    aplicando shift alt O em app.module

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {NgModule} from '@angular/core';
-import {ButtonModule, CardModule, InputTextModule} from "primeng";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule, CardModule, InputTextModule } from "primeng";
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuModule } from 'primeng/menu';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { HomeComponent } from './pages/home/home.component';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './pages/home/home.component';
-import {PerfilComponent} from './components/perfil/perfil.component';
 
-import {LayoutComponent} from './components/layout/layout.component';
-import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {MenuModule} from 'primeng/menu';
-import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PerfilComponent, LayoutComponent, NavBarComponent, BreadcrumbComponent],
