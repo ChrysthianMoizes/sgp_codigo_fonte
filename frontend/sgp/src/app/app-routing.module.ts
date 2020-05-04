5c44ee0f4b37bd86914d426bd6eae623f8aa08bc
Alisson Vighini <alisson.vighini@gmail.com>
2020-05-04 15:34:51 -0300

    Criando bread crumbs

--- frontend/sgp/src/app/app-routing.module.ts
+++ frontend/sgp/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: 'Home',
-    component: HomeComponent,
-    /*children:[{
-      path: 'Perfil'
-      component: PerfilComponent ,
-    }]*/
+    path: '', component: HomeComponent, data:{ breadcrumb: 'home'}
+  },{
+    path: 'layout', component: LayoutComponent, data:{ breadcrumb: 'Layout'},
+    children:[ {path: 'teste',component: HomeComponent, data: {breadcrumb: 'teste'}}]
   }/*,
   {
     path: 'login',
