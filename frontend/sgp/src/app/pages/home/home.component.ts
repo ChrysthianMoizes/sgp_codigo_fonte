032e3fb0c1d2db129f9ac27f351f884a2dc7f278
Arthur Neitzel <arthneitzel@gmail.com>
2020-05-04 13:56:14 -0300

    adição de usuario

--- frontend/sgp/src/app/pages/home/home.component.ts
+++ frontend/sgp/src/app/pages/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  usuario: any;
+  usuario = { nome: 'Teste', id: 1 };
 
   ngOnInit(): void {}
 }
