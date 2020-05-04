c09a319d8df5af1f4ae3ba92a608306575ed500a
Arthur Neitzel <arthneitzel@gmail.com>
2020-05-04 13:29:23 -0300

    Adicionado notfond import

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -1,24 +1,41 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import {NgModule} from '@angular/core';
-import {ButtonModule, CardModule, InputTextModule} from "primeng";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ButtonModule, CardModule, InputTextModule } from 'primeng';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeComponent} from './pages/home/home.component';
-import {PerfilComponent} from './components/perfil/perfil.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-import {LayoutComponent} from './components/layout/layout.component';
-import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {MenuModule} from 'primeng/menu';
-import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MenuModule } from 'primeng/menu';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PerfilComponent, LayoutComponent, NavBarComponent, BreadcrumbComponent],
-  imports: [BrowserModule, AppRoutingModule, CardModule, InputTextModule, ButtonModule, BrowserAnimationsModule, MenuModule, BreadcrumbModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PerfilComponent,
+    LayoutComponent,
+    NavBarComponent,
+    BreadcrumbComponent,
+    NotfoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    MenuModule,
+    BreadcrumbModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
