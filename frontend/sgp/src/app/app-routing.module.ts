2735b1d09816ce9232c5d5fde0ae720fe3ff8987
joseaugustodardengo <joseaugusto.dardengo@gmail.com>
2020-05-05 15:55:37 -0300

    ja/alteração no layout

--- frontend/sgp/src/app/app-routing.module.ts
+++ frontend/sgp/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ReenviarEmailComponent } from './pages/reenviar-email/reenviar-email.component';
 import { ResetarSenhaComponent } from './pages/resetar-senha/resetar-senha.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 const routes: Routes = [
   {
@@ -16,7 +17,12 @@ const routes: Routes = [
   {
     path: 'resetarsenha',
     component: ResetarSenhaComponent
-  },/*,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
+  },
+  /*,
   {
     path: 'login',
     component: },
