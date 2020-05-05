fa99083e29dfeca68ed5f0dabbdaae446b1dd335
Gustavo Zucolotto <49893876+gudeck@users.noreply.github.com>
2020-05-05 14:55:32 -0300

    Time3 (#21)
    * .
    
    * Componente criado
    
    * adicionando breadcrumb
    
    * resetando css global
    
    * add layout component
    
    * importing layout component to app.module
    
    * Crindo breadcrumb component
    
    * alterações em app-rooting e breadcrumb
    
    * gcz/campos e botões organizados usando flexgrid
    
    * making body background greyer
    
    * rm serif from font
    
    * adding profile img to assets
    
    * implemented nav bar
    
    * imported nav bar into app.module
    
    * Crindo breadcrumb component e resolvendo conflitos
    
    * código refatorado, imports organizados
    
    * add importacoes de dependencias
    
    * aplicando shift alt O em app.module
    
    * Adicionado conteudo a home page.
    
    * Criada not found page
    
    * Adicionado notfond import
    
    * add media queries at h1 at nav-bar
    
    * add padding to layout
    
    * adição de estilos a pagina home.
    
    * adição de usuario
    
    * Estilização do notfound
    
    * aumentando tamanho do icone 'menu hamburguer'
    
    * Criando bread crumbs
    
    * add breadcrumb a branch time 3
    
    * comflito de dependencia dupla
    
    * serviços de obter perfil e atualizar registro criados em MOCK
    
    * Correçoes BreadCrumb
    
    * scripts do banco adicionados ao repositório do projeto
    
    * .gitignore filtrando mais coisas
    
    * .
    
    * adicionando cores globais
    
    * limpando app.component.html
    
    * escurencendo laranja da cor primaria
    
    * utilizando variavel na cor da nav bar
    
    Co-authored-by: @chrysthianmoizes <chrysthianmoizes@gmail.com>
    Co-authored-by: Ozaru <diegodavel@gmail.com>
    Co-authored-by: Alisson Vighini <alisson.vighini@gmail.com>
    Co-authored-by: Flávio L <flaviojunior0899@gmail.com>
    Co-authored-by: Arthur Neitzel <arthneitzel@gmail.com>

--- frontend/sgp/src/app/components/perfil/perfil.component.spec.ts
+++ frontend/sgp/src/app/components/perfil/perfil.component.spec.ts
@@ -1,20 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {HomeComponent} from './home.component';
+import {PerfilComponent} from './perfil.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('PerfilComponent', () => {
+  let component: PerfilComponent;
+  let fixture: ComponentFixture<PerfilComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [PerfilComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(PerfilComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
