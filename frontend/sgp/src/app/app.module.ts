c8c0c6e61b84a40f8ffd9dcbf3622f9028ccec2c
Flávio L <flaviojunior0899@gmail.com>
2020-05-06 11:11:07 -0300

    corrigindo imports em app.module

--- frontend/sgp/src/app/app.module.ts
+++ frontend/sgp/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ButtonModule, CardModule, InputTextModule} from 'primeng';
-import {BreadcrumbModule} from 'primeng/breadcrumb';
-import {MenuModule} from 'primeng/menu';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BreadcrumbComponent} from './components/breadcrumb/breadcrumb.component';
-import {LayoutComponent} from './components/layout/layout.component';
-import {NavBarComponent} from './components/nav-bar/nav-bar.component';
-import {PerfilComponent} from './components/perfil/perfil.component';
-import {HomeComponent} from './pages/home/home.component';
-import {NotfoundComponent} from './pages/notfound/notfound.component';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule, CardModule, InputTextModule } from 'primeng';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { MenuModule } from 'primeng/menu';
+import { PaginatorModule } from 'primeng/paginator';
+import { PickListModule } from 'primeng/picklist';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { CadastrarProvaComponent } from './components/cadastrar-prova/cadastrar-prova.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
@@ -23,10 +27,14 @@ import {NotfoundComponent} from './pages/notfound/notfound.component';
     NavBarComponent,
     BreadcrumbComponent,
     NotfoundComponent,
+    CadastrarProvaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PaginatorModule,
+    ReactiveFormsModule,
+    PickListModule,
     CardModule,
     InputTextModule,
     ButtonModule,
