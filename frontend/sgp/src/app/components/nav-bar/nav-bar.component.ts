d2e17b75ae32a6cbf6d75411372ad25d5b3b6165
Tito Stauffer <tito.stauffer@gmail.com>
2020-05-06 11:33:30 -0300

    criando a interface da sidebar

--- frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,20 +1,16 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {MenuItem} from 'primeng/api/menuitem';
+import { Component, Input, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api/menuitem';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-
   items: MenuItem[];
   @Input() title = 'Gestão de Provas - Basis';
-
   ngOnInit(): void {
-    this.items = [
-      {label: 'Logout', command: this.onLogout}
-    ]
+    this.items = [{ label: 'Logout', command: this.onLogout }];
   }
 
   onLogout(event): void {
@@ -22,7 +18,10 @@ export class NavBarComponent implements OnInit {
   }
 
   onClickHamburguerMenu(event): void {
-    // TODO: Adicionar lógica do clique
+    let btn = document.querySelector('#sidebar');
+    let icone = document.querySelector('.pi-bars');
+    console.log(icone);
+    btn.classList.toggle('ativo');
+    icone.classList.toggle('icone');
   }
-
 }
