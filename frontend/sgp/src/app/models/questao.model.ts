57615ba8918bea6ccdf6a9725f9f821854a03431
Flávio L <flaviojunior0899@gmail.com>
2020-05-06 13:51:47 -0300

    criando questao.model

---
+++ frontend/sgp/src/app/models/questao.model.ts
@@ -0,0 +1,10 @@
+export class Questao {
+  id: number;
+  descricao: string;
+  // alternativa1: string;
+  // alternativa2: string;
+  // alternativa3: string;
+  // alternativa4: string;
+  // alternativa5: string;
+  // resposta: number;
+}
