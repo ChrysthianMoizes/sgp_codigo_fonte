b5ff670a6d2fd952ba3cde7d90f483bdbad65ea3
Tito Stauffer <tito.stauffer@gmail.com>
2020-05-06 15:48:36 -0300

    Sidebar e correções

--- frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,3 +1,6 @@
+import { MenuModel } from './../../models/menu.model';
+import { menu } from './../../menu';
+import { AuthService } from './../../services/auth.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api/menuitem';
 
@@ -7,9 +10,14 @@ import { MenuItem } from 'primeng/api/menuitem';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
   items: MenuItem[];
+  menuSideBar: MenuModel[];
+
   @Input() title = 'Gestão de Provas - Basis';
   ngOnInit(): void {
+    this.menuSideBar = menu;
     this.items = [{ label: 'Logout', command: this.onLogout }];
   }
 
@@ -17,10 +25,20 @@ export class NavBarComponent implements OnInit {
     // TODO: Adicionar lógica do logout
   }
 
+  verificaPermissao(permissaoMenu: string): boolean {
+    if (
+      permissaoMenu === this.authService.getPermissaoUsuarioSessionStorage() ||
+      permissaoMenu === ''
+    ) {
+      return true;
+    }
+    return false;
+  }
+
   onClickHamburguerMenu(event): void {
-    let btn = document.querySelector('#sidebar');
+    let btn = document.querySelector('.sidebar');
     let icone = document.querySelector('.pi-bars');
-    console.log(icone);
+
     btn.classList.toggle('ativo');
     icone.classList.toggle('icone');
   }
