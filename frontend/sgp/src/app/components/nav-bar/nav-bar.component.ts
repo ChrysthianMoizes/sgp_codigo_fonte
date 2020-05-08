67be05a3d808ad19c6224924452c2ac6af17d999
Flávio L <flaviojunior0899@gmail.com>
2020-05-08 15:32:35 -0300

    add 'Editar Perfil' on dropdown at navbar

--- frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/sgp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/api/menuitem';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,10 +12,17 @@ export class NavBarComponent implements OnInit {
   items: MenuItem[];
   @Input() title = 'Gestão de Provas - Basis';
 
+  constructor(private router: Router) { }
+
   ngOnInit(): void {
     this.items = [
+      {label: 'Editar Perfil', command: () => this.onEditarPerfil()},
       {label: 'Logout', command: this.onLogout}
     ];
+  }
+
+  onEditarPerfil(): void {
+    this.router.navigateByUrl('/home');
   }
 
   onLogout(event): void {
