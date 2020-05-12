fa4ab75b27ab2b8978760bf36e8b36eebc774f24
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2020-05-12 16:52:27 -0300

    Criação do backend

---
+++ backend/src/main/java/br/com/basis/sgp/dominio/Usuario.java
@@ -0,0 +1,38 @@
+package br.com.basis.sgp.dominio;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "USUARIO")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "NOME", nullable = false)
+    private String nome;
+
+    @Column(name = "CPF", nullable = false)
+    private String cpf;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "SENHA", nullable = false)
+    private String senha;
+
+    @Column(name = "ADMIN", nullable = false)
+    private Integer admin;
+}
---
+++ backend/src/main/java/br/com/basis/sgp/servico/dto/SelectDTO.java
@@ -0,0 +1,13 @@
+package br.com.basis.sgp.servico.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SelectDTO {
+
+    private Long value;
+    private String label;
+
+}
