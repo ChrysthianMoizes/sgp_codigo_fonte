d1a015653cbdd506853f14f110400a43c63eb34a
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2020-05-14 14:45:08 -0300

    backend completo de usuario

--- backend/src/main/java/br/com/basis/sgp/servico/dto/UsuarioDTO.java
+++ backend/src/main/java/br/com/basis/sgp/servico/dto/UsuarioDTO.java
@@ -1,24 +1,16 @@
 package br.com.basis.sgp.servico.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class UsuarioDTO {
 
     private Long id;
     private String nome;
     private String email;
     private String cpf;
+    private Integer admin;
 
-    public UsuarioDTO(Long id, String nome, String email, String cpf) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.cpf = cpf;
-    }
 }
