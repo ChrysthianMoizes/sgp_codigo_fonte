24eb38b01df500a3f8f71f26f55f4a3281a170fb
Tito Stauffer <tito.stauffer@gmail.com>
2020-05-15 14:35:20 -0300

    bugfix

--- frontend/sgp/src/app/pages/usuario/models/usuario.ts
+++ frontend/sgp/src/app/pages/usuario/models/usuario.ts
@@ -1,11 +1,9 @@
 import { Resource } from 'src/app/models/resource.model';
 
 export class Usuario extends Resource {
-
   nome: string;
   cpf: string;
   senha: string;
   email: string;
   admin: boolean;
-
 }
