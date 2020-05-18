5b052a2b10caeaf617464fe989e3243b6be12d09
jeanpandolfi <jean-pandolfi@hotmail.com>
2020-05-18 17:48:08 -0300

    Adicionando model de filtro e layout na tabela

---
+++ frontend/sgp/src/app/pages/questao/models/questao-filtro.model.ts
@@ -0,0 +1,13 @@
+export class QuestaoFiltro {
+  id: number;
+  descricao: string;
+  senioridade: string;
+  tipoQuestao: string;
+
+  constructor(){
+    this.id = null;
+    this.descricao = null;
+    this.senioridade = null;
+    this.tipoQuestao = null;
+  }
+}
