72434aeb9286027708e44b4c84e1a6e452145248
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-05-18 17:13:29 -0300

    filtro por ID adicionado

--- backend/src/main/java/br/com/basis/sgp/servico/filtro/QuestaoFiltro.java
+++ backend/src/main/java/br/com/basis/sgp/servico/filtro/QuestaoFiltro.java
@@ -6,6 +6,7 @@ import br.com.basis.sgp.dominio.Senioridade_;
 import br.com.basis.sgp.dominio.TipoQuestao_;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @Setter
 public class QuestaoFiltro implements EntityFiltro<Questao> {
 
+    private Long id;
     private String descricao;
     private String senioridade;
     private String tipoQuestao;
@@ -30,6 +32,10 @@ public class QuestaoFiltro implements EntityFiltro<Questao> {
 
     private List<Predicate> getPredicates(Root<Questao> root, CriteriaBuilder builder) {
         List<Predicate> predicates = new ArrayList<>();
+        if (ObjectUtils.isNotEmpty(id)) {
+            Predicate predicate = builder.equal(root.get(Questao_.id), id);
+            predicates.add(predicate);
+        }
         if (!StringUtils.isEmpty(descricao)) {
             Predicate predicate = builder.like(root.get(Questao_.descricao), "%" + descricao.toLowerCase() + "%");
             predicates.add(predicate);
