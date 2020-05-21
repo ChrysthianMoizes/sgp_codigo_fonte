b51d13739a90a9e3097c7fab9e3f028b53c50acd
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-05-21 14:10:40 -0300

    correção de bugs visuais e pedidos da pr

--- backend/src/main/java/br/com/basis/sgp/servico/filtro/QuestaoFiltro.java
+++ backend/src/main/java/br/com/basis/sgp/servico/filtro/QuestaoFiltro.java
@@ -1,8 +1,8 @@
 package br.com.basis.sgp.servico.filtro;
 
-//import br.com.basis.sgp.dominio.Questao_;
-//import br.com.basis.sgp.dominio.Senioridade_;
-//import br.com.basis.sgp.dominio.TipoQuestao_;
+import br.com.basis.sgp.dominio.Questao_;
+import br.com.basis.sgp.dominio.Senioridade_;
+import br.com.basis.sgp.dominio.TipoQuestao_;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.commons.lang3.ObjectUtils;
@@ -30,23 +30,23 @@ public class QuestaoFiltro implements EntityFiltro {
     }
 
     private List<Predicate> getPredicates(Root root, CriteriaBuilder builder) {
-        List<Predicate> predicates = new ArrayList<>();
-//        if (ObjectUtils.isNotEmpty(id)) {
-//            Predicate predicate = builder.equal(root.get(Questao_.id), id);
-//            predicates.add(predicate);
-//        }
-//        if (StringUtils.isNotEmpty(descricao)) {
-//            Predicate predicate = builder.like(root.get(Questao_.descricao), "%" + descricao.toLowerCase() + "%");
-//            predicates.add(predicate);
-//        }
-//        if (ObjectUtils.isNotEmpty(senioridade) && ObjectUtils.notEqual(senioridade, 0L)) {
-//            Predicate predicate = builder.equal(root.join("senioridade").get(Senioridade_.ID), senioridade);
-//            predicates.add(predicate);
-//        }
-//        if (ObjectUtils.isNotEmpty(tipoQuestao) && ObjectUtils.notEqual(senioridade, 0L)) {
-//            Predicate predicate = builder.equal(root.join("tipoQuestao").get(TipoQuestao_.ID), tipoQuestao);
-//            predicates.add(predicate);
-//        }
+        List<Predicate> predicates = new ArrayList();
+        if (ObjectUtils.isNotEmpty(id)) {
+            Predicate predicate = builder.equal(root.get(Questao_.id), id);
+            predicates.add(predicate);
+        }
+        if (StringUtils.isNotEmpty(descricao)) {
+            Predicate predicate = builder.like(root.get(Questao_.descricao), "%" + descricao.toLowerCase() + "%");
+            predicates.add(predicate);
+        }
+        if (ObjectUtils.isNotEmpty(senioridade) && ObjectUtils.notEqual(senioridade, 0L)) {
+            Predicate predicate = builder.equal(root.join("senioridade").get(Senioridade_.ID), senioridade);
+            predicates.add(predicate);
+        }
+        if (ObjectUtils.isNotEmpty(tipoQuestao) && ObjectUtils.notEqual(tipoQuestao, 0L)) {
+            Predicate predicate = builder.equal(root.join("tipoQuestao").get(TipoQuestao_.ID), tipoQuestao);
+            predicates.add(predicate);
+        }
         return predicates;
     }
 }
